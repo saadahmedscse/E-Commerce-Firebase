b568cf17c6e0b09474a951062365738555245817
Saad Ahmed <saadahmedscse@gmail.com>
2022-02-14 00:51:13 +0600

    added uid

--- app/src/main/java/com/caffeine/ecommercefirebase/util/Constants.kt
+++ app/src/main/java/com/caffeine/ecommercefirebase/util/Constants.kt
@@ -1,9 +1,17 @@
 package com.caffeine.ecommercefirebase.util
 
+import android.content.Context
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 
 object Constants {
     const val appName = "Rengvo"
     val reference : DatabaseReference = FirebaseDatabase.getInstance().reference.child(appName)
+    val UID = FirebaseAuth.getInstance().uid
+
+    fun getHorizontalLayout(context: Context) : LinearLayoutManager{
+        return LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+    }
 }
