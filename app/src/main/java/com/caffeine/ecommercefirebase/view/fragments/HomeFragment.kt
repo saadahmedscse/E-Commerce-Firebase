a6407059c7a08dc96fd872ed98cf0b8754f03f8b
Saad Ahmed <saadahmedscse@gmail.com>
2022-02-13 10:37:28 +0600

    show categorized products

--- app/src/main/java/com/caffeine/ecommercefirebase/view/fragments/HomeFragment.kt
+++ app/src/main/java/com/caffeine/ecommercefirebase/view/fragments/HomeFragment.kt
@@ -5,21 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.caffeine.ecommercefirebase.R
 import com.caffeine.ecommercefirebase.databinding.FragmentHomeBinding
+import com.caffeine.ecommercefirebase.services.model.ProductDetails
 import com.caffeine.ecommercefirebase.util.Task
 import com.caffeine.ecommercefirebase.view.activities.DashboardActivity
 import com.caffeine.ecommercefirebase.view.adapter.CategoryAdapter
+import com.caffeine.ecommercefirebase.view.adapter.ProductAdapter
 import com.caffeine.ecommercefirebase.viewmodel.ProductViewModel
 
 class HomeFragment : Fragment() {
 
-    private lateinit var binding : FragmentHomeBinding
-    private val productViewModel : ProductViewModel by viewModels()
+    private lateinit var binding: FragmentHomeBinding
+    private val productViewModel: ProductViewModel by viewModels()
+    private lateinit var productDetails: ArrayList<ProductDetails>
+    private lateinit var ForMen: ArrayList<ProductDetails>
+    private lateinit var ForWomen: ArrayList<ProductDetails>
+    private lateinit var ForChild: ArrayList<ProductDetails>
+    private lateinit var Popular: ArrayList<ProductDetails>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,12 +35,27 @@ class HomeFragment : Fragment() {
     ): View? {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
 
-        val categoryLayoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+        val categoryLayoutManager = getLayoutManager()
+        val popularLayoutManager = getLayoutManager()
+        val menLayoutManager = getLayoutManager()
+        val womenLayoutManager = getLayoutManager()
+        val childLayoutManager = getLayoutManager()
+
         binding.categoryRecyclerView.layoutManager = categoryLayoutManager
+        binding.popularRecyclerView.layoutManager = popularLayoutManager
+        binding.menRecyclerView.layoutManager = menLayoutManager
+        binding.womenRecyclerView.layoutManager = womenLayoutManager
+        binding.childRecyclerView.layoutManager = childLayoutManager
+
+        productDetails = ArrayList()
+        ForMen = ArrayList()
+        ForWomen = ArrayList()
+        ForChild = ArrayList()
+        Popular = ArrayList()
 
         productViewModel.getCategories()
         productViewModel.categoryLiveData.observe(viewLifecycleOwner, Observer {
-            when (it){
+            when (it) {
                 is Task.Loading -> {
                     //loading
                 }
@@ -49,6 +72,20 @@ class HomeFragment : Fragment() {
             }
         })
 
+        productViewModel.getAllProducts()
+        productViewModel.allProducts.observe(viewLifecycleOwner) {
+            when (it) {
+                is Task.Loading -> {}
+
+                is Task.Success -> {
+                    productDetails = it.data as ArrayList<ProductDetails>
+                    setAdapters()
+                }
+
+                is Task.Failed -> {}
+            }
+        }
+
         return binding.root
     }
 
@@ -56,5 +93,41 @@ class HomeFragment : Fragment() {
         super.onResume()
 
         (activity as DashboardActivity).setActionBarTitle("Home")
+    }
+
+    private fun getLayoutManager() : LinearLayoutManager{
+        return LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+    }
+
+    private fun setAdapters() {
+        for (obj in productDetails){
+            if (obj.category == "For Men"){
+                ForMen.add(obj)
+            }
+
+            if (obj.category == "For Women"){
+                ForWomen.add(obj)
+            }
+
+            if (obj.category == "For Child"){
+                ForChild.add(obj)
+            }
+
+            if (obj.category == "Summer"){
+                Popular.add(obj)
+            }
+        }
+
+        val menAdapter = ProductAdapter(requireContext(), ForMen)
+        binding.menRecyclerView.adapter = menAdapter
+
+        val womenAdapter = ProductAdapter(requireContext(), ForWomen)
+        binding.womenRecyclerView.adapter = womenAdapter
+
+        val childAdapter = ProductAdapter(requireContext(), ForChild)
+        binding.childRecyclerView.adapter = childAdapter
+
+        val popularAdapter = ProductAdapter(requireContext(), Popular)
+        binding.popularRecyclerView.adapter = popularAdapter
     }
 }
