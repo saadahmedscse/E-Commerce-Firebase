a18d087ac1558ceeaf822b8c5291c4392d305312
Saad Ahmed <saadahmedscse@gmail.com>
2022-02-17 02:12:54 +0600

    added cart icon badge

--- app/src/main/java/com/caffeine/ecommercefirebase/view/activities/DashboardActivity.kt
+++ app/src/main/java/com/caffeine/ecommercefirebase/view/activities/DashboardActivity.kt
@@ -4,7 +4,11 @@ import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
+import android.view.View
+import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.ui.*
 import com.caffeine.ecommercefirebase.R
@@ -14,10 +18,15 @@ import com.caffeine.ecommercefirebase.services.model.Images
 import com.caffeine.ecommercefirebase.services.model.ProductDetails
 import com.caffeine.ecommercefirebase.services.model.Sizes
 import com.caffeine.ecommercefirebase.util.Constants
+import com.caffeine.ecommercefirebase.util.DataState
+import com.caffeine.ecommercefirebase.view.fragments.CartFragment
+import com.caffeine.ecommercefirebase.view.fragments.HomeFragment
+import com.caffeine.ecommercefirebase.viewmodel.CartViewModel
 
 class DashboardActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityDashboardBinding
+    private val cartViewModel : CartViewModel by viewModels()
     private var count = 0
 
     private lateinit var navController: NavController
@@ -37,6 +46,24 @@ class DashboardActivity : AppCompatActivity() {
             }
             else{
                 binding.drawer.openDrawer(Gravity.LEFT)
+            }
+        }
+
+        binding.btnCart.setOnClickListener{
+            when (navController.currentDestination?.id){
+                R.id.homeFragment -> navController.navigate(R.id.home_to_cart)
+                R.id.detailsFragment -> navController.navigate(R.id.details_to_cart)
+                R.id.profileFragment -> navController.navigate(R.id.profile_to_cart)
+            }
+        }
+
+        cartViewModel.getCartItems()
+        cartViewModel.getCarts.observe(this){
+            when (it){
+                is DataState.Success -> {
+                    if (it.data?.size != 0) binding.cartNotification.visibility = View.VISIBLE
+                    else binding.cartNotification.visibility = View.GONE
+                }
             }
         }
     }
