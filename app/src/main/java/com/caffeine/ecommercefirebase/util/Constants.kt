c8e6f6b0b8ef5c546af94c9526cb719b1264617e
Saad Ahmed <saadahmedscse@gmail.com>
2022-02-17 02:13:34 +0600

    added new data state

--- app/src/main/java/com/caffeine/ecommercefirebase/util/Constants.kt
+++ app/src/main/java/com/caffeine/ecommercefirebase/util/Constants.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.caffeine.ecommercefirebase.services.model.CartModel
 import com.caffeine.ecommercefirebase.services.model.ProductDetails
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
@@ -20,5 +21,9 @@ object Constants {
 
     fun getMutableDataStateListOfProductDetails() : MutableLiveData<DataState<List<ProductDetails>>>{
         return MutableLiveData<DataState<List<ProductDetails>>>()
+    }
+
+    fun getMutableDataStateListOfCarts() : MutableLiveData<DataState<List<CartModel>>>{
+        return MutableLiveData<DataState<List<CartModel>>>()
     }
 }
